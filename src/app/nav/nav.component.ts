3eb1d6b69603b3c4ca8e7fb0cac84ff9f6f9f728
Arshad <arshadpakkali@gmail.com>
2020-11-09 00:05:54 +0530

    {update} Added Navigation

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss'],
+})
+export class NavComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => {
+        console.log(result);
+        return result.matches;
+      }),
+      shareReplay()
+    );
+  @ViewChild('drawer') sidenav: any;
+
+  menuItems = [
+    { name: 'home', icon: 'home' },
+    { name: 'about', icon: 'info' },
+    { name: 'projects', icon: 'code' },
+    { name: 'contact', icon: 'share' },
+  ];
+  constructor(private breakpointObserver: BreakpointObserver) {}
+  toogleSidenav(): void {
+    this.sidenav.toggle();
+  }
+}
